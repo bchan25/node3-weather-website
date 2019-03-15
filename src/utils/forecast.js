9792776483f69e3a43622b61332e7127385c5cc0
Benjamin Chan <benchan_2000@hotmail.com>
2019-03-15 16:58:09 +0000

    Added wind speed to forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
          callback('Unable to find location', undefined)
        } else{
          callback(undefined, {
-            message: body.daily.data[0].summary  + " It is currently " + body.currently.temperature + " degress out. There is a " + body.currently.precipProbability + "% chance of rain."
+            message: body.daily.data[0].summary  + ' It is currently ' + body.currently.temperature + ' degress out. Current wind speed ' + body.currently.windSpeed + 'mph. There is a ' + body.currently.precipProbability + '% chance of rain.'
          })
        }
      })
