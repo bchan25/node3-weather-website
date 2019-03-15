0ad1b8584cef59156422961c4a7971d32aa4f1b8
Benjamin Chan <benchan_2000@hotmail.com>
2019-03-15 14:42:52 +0000

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+console.log("Client side JS loaded")
+
+
+
+//Fetch is a Browser (Client only) method and not NODE
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+
+    // Stop form submit reload page
+    e.preventDefault()
+
+    const location = search.value
+    
+    const addressURL = './weather?address=' + location
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch(addressURL).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                messageOne.textContent = data.error
+            } else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+            
+        })
+    })
+
+})
---
+++ src/app.js
@@ -0,0 +1,113 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
+const app = express()
+const port = 3000
+
+// Define Paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Set up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+// Setup Static Directory to serve
+app.use(express.static(publicDirectoryPath))
+
+// Dynamic HTML 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ben'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Ben'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is some helpful text',
+        title: 'Help',
+        name: 'Ben'
+    })
+})
+
+// Query String
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide a valid address'
+        })
+    } else{
+        geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+          
+          if(error){
+            return res.send({ error })
+          }
+
+          forecast(latitude, longitude, (error, {message}) => {
+            if(error){
+              return res.send({ error })
+            }
+
+            res.send({
+                forecast: message,
+                location,
+                address: req.query.address
+            })
+            
+          })
+        
+        })
+      }
+
+
+    
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+
+    res.send({
+        products: []
+    })
+})
+
+// Must come LAST 
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Ben',
+        errorMessage: 'Help article note found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Ben',
+        errorMessage: 'Page note found'
+    })
+})
+
+// Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
